f97d185dd02410362282b00bc853ff1ecc455160
huzing2524 <826956010@qq.com>
2019-12-16 06:24:51 -0800

    update 2019.12.16

--- utils/util.js
+++ utils/util.js
@@ -2,11 +2,11 @@ const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+  // const hour = date.getHours()
+  // const minute = date.getMinutes()
+  // const second = date.getSeconds()   + ' ' + [hour, minute, second].map(formatNumber).join(':')写在下面的
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('/') 
 }
 
 const formatNumber = n => {
