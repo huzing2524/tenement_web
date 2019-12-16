9afb950e7f84a7d0e13352599b13db45673093d5
huzing2524 <huzing2524@github.com>
2019-12-16 06:24:51 -0800

    update 2019.12.16

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,187 +1,93 @@
 Page({
-  phonecall: function (e) {
-    console.log(e.target.dataset.sss)
-    wx.makePhoneCall({
-      phoneNumber: this.data.array[e.target.dataset.sss].num
-    })
-  },
-  // getMarkers() {
-  //   wx.request({
-  //     url: 'http://127.0.0.1:8868/add?a=1&b=2',
-  //     method: 'get',
-  //     // success: this.setData({ markers })
-  //     success(){
-  //       console.log("可以访问")
-  //     } 
-  //   })
-  // },
+  //数据
   data: {
-    // phonecalls: '13396089753',
-    pagenum: 1,
-    imglist: ['http://demo.sc.chinaz.com/Files/DownLoad/webjs1/201801/jiaoben5647/img/5.jpg', 'http://demo.sc.chinaz.com/Files/DownLoad/webjs1/201801/jiaoben5647/img/1.jpg', 'http://demo.sc.chinaz.com/Files/DownLoad/webjs1/201801/jiaoben5647/img/2.jpg'],
-    array: [{
-      text: '广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1',
-      src: [{img:'../show_img/1.jpg'},
-        {img:'../show_img/2.jpg'},
-        {img:'../show_img/3.jpg'},
-        { img: '../show_img/4.jpg' },
-        { img: '../show_img/5.jpg' }],
-      address:'地点',
-      event:'事件',
-      date:'时间',
-      person:'人物',
-      price:'价格',
-      tel: '电话',
-      num:'13396089751'
-    },{
-        text: '广告展示图2广告展示图2广告展示图2广告展示图2广告展示图2广告展示图2广告展示图2',
-        src: [{ img: '../show_img/1.jpg' },
-        { img: '../show_img/2.jpg' },
-        { img: '../show_img/3.jpg' },
-        { img: '../show_img/4.jpg' },
-        { img: '../show_img/5.jpg' }],
-        address: '地点',
-        event: '事件',
-        date: '时间',
-        person: '人物',
-        price: '价格',
-        tel: '电话',
-        num: '13396089752'
-      }, {
-        text: '广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1',
-        src: [{ img: '../show_img/1.jpg' },
-        { img: '../show_img/2.jpg' },
-        { img: '../show_img/3.jpg' },
-        { img: '../show_img/4.jpg' },
-        { img: '../show_img/5.jpg' }],
-        address: '地点',
-        event: '事件',
-        date: '时间',
-        person: '人物',
-        price: '价格',
-        tel: '电话',
-        num: '13396089753'
-      }, {
-        text: '广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1',
-        src: [{ img: '../show_img/1.jpg' },
-        { img: '../show_img/2.jpg' },
-        { img: '../show_img/3.jpg' },
-        { img: '../show_img/4.jpg' },
-        { img: '../show_img/5.jpg' }],
-        address: '地点',
-        event: '事件',
-        date: '时间',
-        person: '人物',
-        price: '价格',
-        tel: '电话',
-        num: '13396089754'
-      }, {
-        text: '广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1',
-        src: [{ img: '../show_img/1.jpg' },
-        { img: '../show_img/2.jpg' },
-        { img: '../show_img/3.jpg' },
-        { img: '../show_img/4.jpg' },
-        { img: '../show_img/5.jpg' }],
-        address: '地点',
-        event: '事件',
-        date: '时间',
-        person: '人物',
-        price: '价格',
-        tel: '电话',
-        num: '1339608975'
-      }, {
-        text: '广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1',
-        src: [{ img: '../show_img/1.jpg' },
-        { img: '../show_img/2.jpg' },
-        { img: '../show_img/3.jpg' },
-        { img: '../show_img/4.jpg' },
-        { img: '../show_img/5.jpg' }],
-        address: '地点',
-        event: '事件',
-        date: '时间',
-        person: '人物',
-        price: '价格',
-        tel: '电话',
-        num: '13396089756'
-      }, {
-        text: '广告展示图last广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1广告展示图1',
-        src: [{ img: '../show_img/1.jpg' },
-        { img: '../show_img/2.jpg' },
-        { img: '../show_img/3.jpg' },
-        { img: '../show_img/4.jpg' },
-        { img: '../show_img/5.jpg' }],
-        address: '地点',
-        event: '事件',
-        date: '时间',
-        person: '人物',
-        price: '价格',
-        tel: '电话',
-        num: '13396089757'
-      }],  
+    psw: 88888,
+    num: 53869,
+    index_data: [],
   },
-  bt_click(){
-    console.log("已点击")
-    wx.navigateTo({
-      url: '../publish/publish' 
+  //打电话
+  phonecall: function(e) {
+    // console.log(e.target.dataset.sss)
+    wx.makePhoneCall({
+      phoneNumber: this.data.index_data[e.target.dataset.sss].contact
     })
   },
-  onShareAppMessage: function (ops) {
+  //首次进入加载数据
+  onLoad: function() {
+    this.loadProduct2()
+  },
+  //校验密码的正确性
+  pass: {},
+  password: function(e) {
+    this.pass.word = e.detail.value
+  },
+  bt_click() {
+    wx.navigateTo({
+      url: '../publish/publish'
+    })
+  },
+  onShareAppMessage: function(ops) {
     if (ops.from === 'button') {
       // 来自页面内转发按钮
       console.log(ops.target)
     }
     return {
-      title: 'xx小程序',
+      title: '餐饮业小程序',
       path: 'pages/index/index',
-      success: function (res) {
+      success: function(res) {
         // 转发成功
         console.log("转发成功:" + JSON.stringify(res));
       },
-      fail: function (res) {
+      fail: function(res) {
         // 转发失败
         console.log("转发失败:" + JSON.stringify(res));
       }
     }
 
   },
-  loadProduct2: function () {
+  //请求网络处理函数，重置数据
+  loadProduct2: function() {
     var that = this;
     wx.request({
-      url: 'https://xxx/?page=',
+      url: 'http://www.luoliming.xyz/page_list',
       method: 'get',
       // success: this.setData({ markers })
-      success: function (res) {
-  //       // 回调函数
-  //       var moment_list = that.data.moment;
-  //       const oldData = that.data.moment;
-  //       that.setData({
-  //         moment: oldData.concat(res.data.data)
-  //       })
-  //       // 隐藏加载框
-  //       wx.hideLoading();
-      }
-    })//发起网络请求。使用前请先阅读说明。
+      success: function(res) {
+
+        // console.log(res.data)
+        // 回调函数
+        // var array_list = that.data.index_data;
+        // const oldData = that.data.index_data; oldData.concat(res.data)新老数据连接需要处理
+        that.setData({
+          index_data: res.data
+        });
+        
+        // 隐藏加载框
+        wx.hideLoading();
+      },
+      fail: function() {}
+    }) //发起网络请求。使用前请先阅读说明。
   },
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
     console.log("下拉");
 
     wx.setNavigationBarTitle({
       title: '刷新中……'
-    })//动态设置当前页面的标题。
+    }) //动态设置当前页面的标题。
 
-    wx.showNavigationBarLoading();//在当前页面显示导航条加载动画。
+    wx.showNavigationBarLoading(); //在当前页面显示导航条加载动画。
 
-    this.loadProduct2();//重新加载产品信息
+    this.loadProduct2(); //重新加载产品信息
 
-    wx.hideNavigationBarLoading();//隐藏导航条加载动画。
+    wx.hideNavigationBarLoading(); //隐藏导航条加载动画。
 
-    wx.stopPullDownRefresh();//停止当前页面下拉刷新。
+    wx.stopPullDownRefresh(); //停止当前页面下拉刷新。
 
     console.log("关闭");
 
     wx.setNavigationBarTitle({
-      title: '小明同学'
-    })//动态设置当前页面的标题。
+      title: '高旺能源欢迎您'
+    }) //动态设置当前页面的标题。
   },
   // onReachBottom: function () {
   //   console.log("上滑")
@@ -193,7 +99,7 @@ Page({
   //   setTimeout(function(){
   //     wx.hideLoading()
   //   },2000)
-    
+
   //   // 页数+1
   //   page = page + 1;
   //   wx.request({
@@ -215,21 +121,71 @@ Page({
   //     }
   //   })
   // },
-  topic_preview: function (e) {
-    // var that = this;
-    // var id = e.currentTarget.dataset.id;
-    // var url = e.currentTarget.dataset.url;
-    // var previewImgArr = [];
-    // //通过循环在数据链里面找到和这个id相同的这一组数据，然后再取出这一组数据当中的图片
-    // var data = that.data.topic_recomData;
-    // for (var i in data) {
-    //   if (id == data[i].id) {
-    //     previewImgArr = data[i].pic;
-    //   }
-    // }
+  //预览图片
+  topic_preview: function(e) {
+    var that = this;
+    var id = e.currentTarget.dataset.id;
+    console.log(id)
+    console.log(that.data.index_data)
+    var previewImgArr = [];
+    for (var i in that.data.index_data) {
+      if (id == that.data.index_data[i].uuid) {
+        console.log("into")
+        var current_img = that.data.index_data[i].images[0].img;
+        previewImgArr = that.data.index_data[i].images;
+      }
+    }
     wx.previewImage({
-      current: 'https://www.baidu.com/img/bd_logo1.png', // 当前显示图片的http链接
-      urls: this.data.imglist// 需要预览的图片http链接列表
+      current: current_img, // 当前显示图片的http链接
+      urls: previewImgArr // 需要预览的图片http链接列表
     })
   },
+  //二维码预览识别跳转
+  click: function() {
+    wx.previewImage({
+      urls: ['https://huzing2524.oss-cn-shenzhen.aliyuncs.com/tenement/prod/%E8%BD%AC%E8%A1%8C.jpg']
+    })
+    // wx.navigateToMiniProgram({
+    //   appId: 'wxfb1ff0f3u0abe913', // 要跳转的小程序的appid
+    //   path: 'page/index/index', // 跳转的目标页面
+    //   extarData: {
+    //     open: 'auth'
+    //   },
+    //   success(res) {
+    //     // 打开成功  
+    //     console.log("跳转成功")
+    //   },
+    //   fail(res){
+    //     console.log("fail")
+    //     console.log(res)
+    //   }
+    // }) 
+  },
+  my_deleteinfo: {},
+  //校验密码删除信息
+  del_info: function(e) {
+    let that = this
+    if (that.pass.word == '高旺环保88888') {
+      that.my_deleteinfo.uuid = e.target.dataset.aaa
+      // console.log(e.target.dataset.aaa)//找寻需要删除的信息的id值
+      // console.log("密码正确")
+      // console.log(that.my_deleteinfo)
+      wx.request({
+        url: 'http://192.168.13.128:8000/info_delete',
+        data: that.my_deleteinfo,
+        method: "delete",
+        success: function(res) {
+          that.loadProduct2()
+        }
+      })
+    } else {
+      console.log("密码错误")
+      that.setData({
+        'value': ''
+      })
+      that.setData({
+        'psw': '请重新输入'
+      })
+    }
+  }
 })
--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -1,28 +1,94 @@
 //logs.js
 // pages/publish/publish.js
-
+var idx = require('../index/index.js')
 var util = require('../../utils/util.js')
+const app = getApp()
+
 Page({
   data: {
+    uploaderList: [],
+    uploaderNum: 0,
+    showUpload: true,
+
+    publicinfo: "发 布",
     swiperIndex: 0,
     image: [],
-    Explain: "填写您的具体需求title0",
-    Explain1:'填写您的具体需求title1',
-    Explain2: '填写您的具体需求title2',
-    Explain4: '填写您的具体需求title4',
-    Explain5: '填写您的具体需求title5',
-    Explain6: '填写您的具体需求title6',
-    flag:true,
-    imglist: "../../resource/img/sell.png"
+    title: '最多输入30字！',
+    address: '最多输入60字！',
+    event: '最多输入100字！',
+    name: '最多输入30字！',
+    price: '保留2位小数！',
+    contact: '请填写您的手机号！',
+    flag: true
+  },
+
+  // 删除图片
+  clearImg: function(e) {
+    var nowList = []; //新数据
+    var uploaderList = this.data.uploaderList; //原数据
+
+    for (let i = 0; i < uploaderList.length; i++) {
+      if (i == e.currentTarget.dataset.index) {
+        continue;
+      } else {
+        nowList.push(uploaderList[i])
+      }
+    }
+    this.setData({
+      uploaderNum: this.data.uploaderNum - 1,
+      uploaderList: nowList,
+      showUpload: true
+    })
+  },
+  //展示图片
+  showImg: function(e) {
+    var that = this;
+    wx.previewImage({
+      urls: that.data.uploaderList,
+      current: that.data.uploaderList[e.currentTarget.dataset.index]
+    })
+  },
+  //上传图片
+  upload: function(e) {
+    var that = this;
+    wx.chooseImage({
+      count: 5 - that.data.uploaderNum, // 默认5
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function(res) {
+        console.log(res)
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        let tempFilePaths = res.tempFilePaths;
+        let uploaderList = that.data.uploaderList.concat(tempFilePaths);
+        if (uploaderList.length == 5) {
+          that.setData({
+            showUpload: false
+          })
+        }
+        that.setData({
+          uploaderList: uploaderList,
+          uploaderNum: uploaderList.length,
+        })
+        console.log(uploaderList.length)
+        console.log(uploaderList)
+      }
+    })
+  },
+
+  chooseItem: function(e) {
+    wx.navigateTo({
+      url: '../upload/upload',
+    })
   },
   swiperChange(e) {
     var that = this;
     that.setData({
-      swiperIndex: e.detail.current,           /*定义当前数据的swiperIndex等于当前数据的current*/
+      swiperIndex: e.detail.current,
+      /*定义当前数据的swiperIndex等于当前数据的current*/
     })
   },
   info: {
-    pic:[]
+    images: []
   },
   handleinfo(e) {
     this.info.title = e.detail.value
@@ -31,46 +97,98 @@ Page({
     this.info.address = e.detail.value
   },
   handleinfo2(e) {
-    this.info.incident = e.detail.value
+    this.info.event = e.detail.value
   },
   handleinfo4(e) {
     this.info.name = e.detail.value
   },
   handleinfo5(e) {
-    this.info.price = e.detail.value
+    var price = e.detail.value
+    this.info.price = parseFloat(price)
   },
   handleinfo6(e) {
-    this.info.tel = e.detail.value
+    var contact = e.detail.value
+    this.info.contact = contact
   },
   handlepic(e) {
-    this.info.pic.push(e)
+    this.info.images.push(e)
   },
   handleClick() {
-    var _this = this;
-    console.log(this.info)
-    wx.request({
-      url: 'http://localhost:3000/list',
-      data: this.info,
-      method: "post",
-      success: this.handlePublishSuccess.bind(this)
-    })
-  }, 
+    let that = this;
+    console.log(that.info)
+
+    if (that.info.title && that.info.address && that.info.event && that.info.name && that.info.price && that.info.contact) {
+      wx.request({
+        url: 'http://www.luoliming.xyz/publication_info',
+        data: that.info,
+        method: "post",
+        success: function(res) {
+          that.handlePublishSuccess()
+          if (res.statusCode == '400') {
+            wx.showModal({
+              title: '提示',
+              content: res.data.errmsg,
+              showCancel: false,
+              success: function(res) {
+                if (res.confirm) {
+                  wx.redirectTo({
+                    url: '/index',
+                  })
+                }
+              }
+            })
+          } else if (res.statusCode == '200') {
+            // wx.navigateTo({
+            //   url: '../index/index',
+            // })
+            wx.showModal({
+              title: '提示',
+              content: '发布成功！',
+              showCancel: false,
+              success: function(res) {
+                if (res.confirm) {
+                  wx.redirectTo({
+                    url: '../index/index',
+                  })
+                }
+              }
+            })
+          }
+        },
+        fail: function() {
+          console.log("发布请求失败")
+
+
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '请填写完整内容！',
+        showCancel: false
+      })
+    }
+  },
   handlePublishSuccess(data) {
     this.setData({
-      flag:false
+      flag: false
     })
   },
-  handleHome(){
-    wx.navigateTo({
+
+
+
+  /* handleHome() {
+    wx.navigateBack({
       url: '../index/index',
     })
-  },
-  uploadpic: function () {
+  }, */
+
+  /* uploadpic: function() {
     let that = this;
     wx.showActionSheet({
       itemList: ['从相册中选择', '拍照'],
       itemColor: "#f7982a",
-      success: function (res) {
+      success: function(res) {
         if (!res.cancel) {
           if (res.tapIndex == 0) {
             that.chooseWxImage('album')
@@ -80,8 +198,9 @@ Page({
         }
       }
     })
-  },
-  chooseWxImage: function (type) {
+  }, */
+
+  /* chooseWxImage: function(type) {
     var that = this;
     wx.chooseImage({
       count: 5,
@@ -89,13 +208,29 @@ Page({
       sourceType: [type],
       success(res) {
         const tempFilePaths = res.tempFilePaths
-        console.warn(tempFilePaths, 'tempFilePaths')
-        
+        // console.warn(tempFilePaths, 'tempFilePaths')
+        // console.warn(tempFilePaths.length)
+        for (let i = 0; i < tempFilePaths.length; i++) {
+          wx.getFileSystemManager().readFile({
+            filePath: res.tempFilePaths[i], //选择图片返回的相对路径
+            encoding: 'base64', //编码格式
+            success: res => { //成功的回调
+              that.handlepic(res.data)
+            }
+          })
+        }
+
         that.setData({
           image: [...that.data.image, ...tempFilePaths]
         })
-        that.handlepic(tempFilePaths)
       }
     })
-  },
+  }, */
+
+  /* admin: function(e) {
+    wx.previewImage({
+      current: "", // 当前显示图片的http链接
+      urls: ['https://huzing2524.oss-cn-shenzhen.aliyuncs.com/tenement/test/default.jpg'] // 需要预览的图片http链接列表
+    })
+  } */
 })
